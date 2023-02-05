fd47e100f8f28743ec0accf2d36ec31bee487c63
Adrian Nads <marcelonads@gmail.com>
2023-02-05 15:48:54 +0800

    ADDED a ThemeContext
    DONE for the toggle theme button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import './css/main.css';
 
+import { createContext, useState } from 'react';
 import Navbar from './components/main_comp/Navbar';
 import Main from './components/main_comp/Main';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+
+  const [isNightMode, setNightMode] = useState(false);
+
   return (
     <div>
-      <Navbar />
-      <Main />      
+      <ThemeContext.Provider value={{isNightMode, setNightMode}}>
+        <Navbar />
+        <Main />      
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/components/main_comp/HeroSection.jsx
+++ src/components/main_comp/HeroSection.jsx
@@ -3,7 +3,7 @@ import '../../css/main.css';
 
 function HeroSection() {
   return (
-     <div className="hero-page container-fluid">
+     <div className="hero-page container">
         <div className="hero-content row justify-content-center">
           <div className="col align-self-center">
             <h1 id='hero--heading' className="display-3 text-center text-lg-start text-dark">I'm Adrian Nads</h1>
