4f4697f74cf731f0fb97e7b0fd36763fc2880ba0
Adrian Nads <marcelonads@gmail.com>
2023-02-06 15:00:39 +0800

    SETUP the dark mode in skills section

--- src/components/main_comp/About.jsx
+++ src/components/main_comp/About.jsx
@@ -12,55 +12,55 @@ function About() {
         <div className='section__title'>
           <h1  className={`display-5 text-center m-0 fw-bold ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>About</h1>
           <p className={`section--heading__divider mt-0 text-center ${isNightMode ? "text-contrastColor divider__darkmode" : "text-dark divider__daymode"}`}>N</p>
-          <hr />
+         <hr className={`${isNightMode ? "border-darkModeAccentColor" : ""}`}/>
         </div>
 
         <div className="container">
           <div className="row align-items-center">
-            <div className="col-12 col-lg-6 text-center align-self-start">
+            <div className="col-12 col-xl-6 text-center align-self-start">
               <img
                 src="/images/About Photo Square.jpg"
                 alt=""
-                className="img-fluid d-block d-lg-none"
+                className="img-fluid d-block d-xl-none"
               />
               <img
                 src="/images/About Photo.jpg"
                 alt=""
-                className="img-fluid d-none d-lg-block"
+                className="img-fluid d-none d-xl-block"
               />
             </div>
             
-            <div className="col-12 col-lg-6">
-              <h1 className={`text-center text-lg-start mt-3 mt-lg-0 font-montserrat-medium ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>
+            <div className="col-12 col-xl-6">
+              <h1 className={`text-center text-xl-start mt-3 mt-xl-0 font-montserrat-medium ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>
                 Hello! I'm Adrian Nads
               </h1>
 
-              <p id='about--paragraph' className={`text-center text-lg-start ${isNightMode ? "text-contrastColor" : "text-bodyTextColor"}`}>
+              <p id='about--paragraph' className={`text-center text-xl-start ${isNightMode ? "text-contrastColor" : "text-bodyTextColor"}`}>
                 I am Adrian Nads L. Marcelo taking a Bachelor of Science in Computer Science, and currently in my senior year at Pamantasan Ng Cabuyao (University of Cabuyao). A passionate, motivated, and solution-oriented Web Developer who loves creating personal projects, connecting to like-minded developers, and constantly learning new skills. I started learning programming in 2019 during my first year in college. I am actively volunteering to code or program some of our school projects to learn and enhance my skills. I also realized that I love creating user interfaces when doing projects in Java Swing, and that made me start to self-learn Web Development through the help of Youtube and other free programming resources. We all know that there's so much to learn in the tech industry, but I am eager and willing to learn new concepts and knowledge, and hoping to collaborate with other developers. I am also open to new opportunities that will come along the way.
               </p>
 
               <ListGroup>
                 <ListGroup.Item className={`${isNightMode ? 'bg-dark border' : 'bg-dark'}`}>
-                  <h3 className={`fst-italic text-center text-lg-start fs-3 ${isNightMode ? 'text-darkModeAccentColor' : 'text-contrastColor'}`}>Relevant Experiences/Activities</h3>
+                  <h3 className={`fst-italic text-center text-xl-start fs-3 ${isNightMode ? 'text-darkModeAccentColor' : 'text-contrastColor'}`}>Relevant Experiences/Activities</h3>
                 </ListGroup.Item>
                 <ListGroup.Item className={`${isNightMode && 'bg-dark border'}`}>
-                  <h3 className={`fs-4 p-0 m-0 text-center text-lg-start ${isNightMode && 'text-contrastColor'}`}>Exploring Computer Science Program Webinar</h3>
-                  <p className="fs-6 fst-italic text-muted text-center text-lg-start">by Association of Computer Science Students on September 03, 2022</p>
-                </ListGroup.Item>
-
-                <ListGroup.Item className={`${isNightMode && 'bg-dark border'}`}>
-                  <h3 className={`fs-4 p-0 m-0 text-center text-lg-start ${isNightMode && 'text-contrastColor'}`}>How to start in Web Development from scratch</h3>
-                  <p className="fs-6 fst-italic text-muted text-center text-lg-start">by Refocus on September 28, 2022</p>
+                  <h3 className={`fs-4 p-0 m-0 text-center text-xl-start ${isNightMode && 'text-contrastColor'}`}>Exploring Computer Science Program Webinar</h3>
+                  <p className="fs-6 fst-italic text-muted text-center text-xl-start">by Association of Computer Science Students on September 03, 2022</p>
                 </ListGroup.Item>
 
                 <ListGroup.Item className={`${isNightMode && 'bg-dark border'}`}>
-                  <h3 className={`fs-4 p-0 m-0 text-center text-lg-start ${isNightMode && 'text-contrastColor'}`}>Zuitt Free Coding Bootcamp</h3>
-                  <p className="fs-6 fst-italic text-muted text-center text-lg-start">by Zuitt Coding Bootcamp on January 14, 2023</p>
+                  <h3 className={`fs-4 p-0 m-0 text-center text-xl-start ${isNightMode && 'text-contrastColor'}`}>How to start in Web Development from scratch</h3>
+                  <p className="fs-6 fst-italic text-muted text-center text-xl-start">by Refocus on September 28, 2022</p>
                 </ListGroup.Item>
 
                 <ListGroup.Item className={`${isNightMode && 'bg-dark border'}`}>
-                  <p className={`fs-4 p-0 m-0 text-center text-lg-start ${isNightMode && 'text-contrastColor'}`}>Finalist in Programming Competition</p>
-                  <p className="fs-6 fst-italic text-muted text-center text-lg-start">by Association of Computer Science Students</p>
+                  <h3 className={`fs-4 p-0 m-0 text-center text-xl-start ${isNightMode && 'text-contrastColor'}`}>Zuitt Free Coding Bootcamp</h3>
+                  <p className="fs-6 fst-italic text-muted text-center text-xl-start">by Zuitt Coding Bootcamp on January 14, 2023</p>
+                </ListGroup.Item>
+
+                <ListGroup.Item className={`${isNightMode && 'bg-dark border'}`}>
+                  <p className={`fs-4 p-0 m-0 text-center text-xl-start ${isNightMode && 'text-contrastColor'}`}>Finalist in Programming Competition</p>
+                  <p className="fs-6 fst-italic text-muted text-center text-xl-start">by Association of Computer Science Students</p>
                 </ListGroup.Item>
               </ListGroup>
             </div>
--- src/components/main_comp/Skills.jsx
+++ src/components/main_comp/Skills.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Container, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 import ProjectTechnology from '../sub_comp/ProjectTechnology';
 
 import '../../css/main.css';
 
+import { ThemeContext } from '../../App';
+
 function Skills() {
+
+  const { isNightMode } = useContext(ThemeContext);
   return (
     <Container id="scrollspySkills" className="my-8 skills">
 
         <div>
-            <h1 className='display-5 text-center mt-5 fw-bold text-dark'>Skills and Technologies</h1>
-            <p className='section--heading__divider'>N</p>
-            <hr />
+            <h1 className={`display-5 text-center mt-5 fw-bold ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>Skills and Technologies</h1>
+            <p className={`section--heading__divider mt-0 text-center ${isNightMode ? "text-contrastColor divider__darkmode" : "text-dark divider__daymode"}`}>N</p>
+            <hr className={`${isNightMode ? "border-darkModeAccentColor" : ""}`}/>
         </div>
         
 
-        <Row className="row-cols-1 row-cols-lg-3">
+        <Row className="row-cols-1 row-cols-xl-3">
             <Col>
                 <div className='d-flex flex-column align-items-center'>
                     <img className="img-fluid m-0 p-0" src="/images/Teamwork.png" alt="" />
-                    <h2 className="fs-1 font-montserrat-medium text-dark">Teamwork</h2>
-                    <p className="text-center font-montserrat text-bodyTextColor">
+                    <h2 className={`fs-1 font-montserrat-medium ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>Teamwork</h2>
+                    <p className={`text-center font-montserrat ${isNightMode ? "text-contrastColor" : "text-bodyTextColor"}`}>
                       A group can achieve more than a person trying to work alone. Working together is more efficient than doing tasks in parallel.
                     </p>
                 </div>
@@ -29,8 +33,8 @@ function Skills() {
             <Col>
                 <div className='d-flex flex-column align-items-center'>
                     <img className="img-fluid" src="/images/Communication.png" alt="" />
-                    <h2 className="fs-1 text-center font-montserrat-medium text-dark">Communication</h2>
-                    <p className="text-center font-montserrat text-bodyTextColor">
+                    <h2 className={`fs-1 font-montserrat-medium ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>Communication</h2>
+                    <p className={`text-center font-montserrat ${isNightMode ? "text-contrastColor" : "text-bodyTextColor"}`}>
                       We all know that there are many jargons in a world of technology, and being able to explain and share your knowledge and ideas are really important in working in a larger team or organization.
                     </p>
                 </div>
@@ -38,8 +42,8 @@ function Skills() {
             <Col>
                 <div className='d-flex flex-column align-items-center'>
                     <img className="img-fluid" src="/images/Problem-Solving.png" alt="" />
-                    <h2 className="fs-1 text-center font-montserrat-medium text-dark">Problem-Solving</h2>
-                    <p className="text-center font-montserrat">
+                    <h2 className={`fs-1 font-montserrat-medium ${isNightMode ? "text-darkModeAccentColor" : "text-dark"}`}>Problem-Solving</h2>
+                    <p className={`text-center font-montserrat ${isNightMode ? "text-contrastColor" : "text-bodyTextColor"}`}>
                       Developing applications is not just for the user, but it is also a solution to a problem. Through my skillset, I am excited to contribute to solve a particular problem.
                     </p>
                 </div>
